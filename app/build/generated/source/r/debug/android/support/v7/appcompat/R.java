d43653d404ce4d0f385d3ade0921ef2c6f2510bc
amitiwary999 <tiwarya9@gmail.com>
2017-02-12 00:10:24 +0530

    improving features

--- app/build/generated/source/r/debug/android/support/v7/appcompat/R.java
+++ app/build/generated/source/r/debug/android/support/v7/appcompat/R.java
@@ -524,18 +524,18 @@ public final class R {
 		public static final int abc_textfield_search_default_mtrl_alpha = 0x7f020050;
 		public static final int abc_textfield_search_material = 0x7f020051;
 		public static final int abc_vector_test = 0x7f020052;
-		public static final int notification_action_background = 0x7f0200fa;
-		public static final int notification_bg = 0x7f0200fb;
-		public static final int notification_bg_low = 0x7f0200fc;
-		public static final int notification_bg_low_normal = 0x7f0200fd;
-		public static final int notification_bg_low_pressed = 0x7f0200fe;
-		public static final int notification_bg_normal = 0x7f0200ff;
-		public static final int notification_bg_normal_pressed = 0x7f020100;
-		public static final int notification_icon_background = 0x7f020101;
-		public static final int notification_template_icon_bg = 0x7f020117;
-		public static final int notification_template_icon_low_bg = 0x7f020118;
-		public static final int notification_tile_bg = 0x7f020102;
-		public static final int notify_panel_notification_icon_bg = 0x7f020104;
+		public static final int notification_action_background = 0x7f0200ff;
+		public static final int notification_bg = 0x7f020100;
+		public static final int notification_bg_low = 0x7f020101;
+		public static final int notification_bg_low_normal = 0x7f020102;
+		public static final int notification_bg_low_pressed = 0x7f020103;
+		public static final int notification_bg_normal = 0x7f020104;
+		public static final int notification_bg_normal_pressed = 0x7f020105;
+		public static final int notification_icon_background = 0x7f020106;
+		public static final int notification_template_icon_bg = 0x7f02011c;
+		public static final int notification_template_icon_low_bg = 0x7f02011d;
+		public static final int notification_tile_bg = 0x7f020107;
+		public static final int notify_panel_notification_icon_bg = 0x7f020109;
 	}
 	public static final class id {
 		public static final int action0 = 0x7f100111;
--- app/build/generated/source/r/debug/com/afollestad/materialdialogs/R.java
+++ app/build/generated/source/r/debug/com/afollestad/materialdialogs/R.java
@@ -573,20 +573,20 @@ public final class R {
 		public static final int abc_textfield_search_activated_mtrl_alpha = 0x7f02004f;
 		public static final int abc_textfield_search_default_mtrl_alpha = 0x7f020050;
 		public static final int abc_textfield_search_material = 0x7f020051;
-		public static final int md_btn_selected = 0x7f0200e0;
-		public static final int md_btn_selected_dark = 0x7f0200e1;
-		public static final int md_btn_selector = 0x7f0200e2;
-		public static final int md_btn_selector_dark = 0x7f0200e3;
-		public static final int md_btn_selector_ripple = 0x7f0200e4;
-		public static final int md_btn_selector_ripple_dark = 0x7f0200e5;
-		public static final int md_btn_shape = 0x7f0200e6;
-		public static final int md_item_selected = 0x7f0200e7;
-		public static final int md_item_selected_dark = 0x7f0200e8;
-		public static final int md_nav_back = 0x7f0200e9;
-		public static final int md_selector = 0x7f0200ea;
-		public static final int md_selector_dark = 0x7f0200eb;
-		public static final int md_transparent = 0x7f0200ec;
-		public static final int notification_template_icon_bg = 0x7f020117;
+		public static final int md_btn_selected = 0x7f0200e5;
+		public static final int md_btn_selected_dark = 0x7f0200e6;
+		public static final int md_btn_selector = 0x7f0200e7;
+		public static final int md_btn_selector_dark = 0x7f0200e8;
+		public static final int md_btn_selector_ripple = 0x7f0200e9;
+		public static final int md_btn_selector_ripple_dark = 0x7f0200ea;
+		public static final int md_btn_shape = 0x7f0200eb;
+		public static final int md_item_selected = 0x7f0200ec;
+		public static final int md_item_selected_dark = 0x7f0200ed;
+		public static final int md_nav_back = 0x7f0200ee;
+		public static final int md_selector = 0x7f0200ef;
+		public static final int md_selector_dark = 0x7f0200f0;
+		public static final int md_transparent = 0x7f0200f1;
+		public static final int notification_template_icon_bg = 0x7f02011c;
 	}
 	public static final class id {
 		public static final int action0 = 0x7f100111;
