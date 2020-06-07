fbe225ba5dff7ea0696d26280d413e8fd9f498e9
amitiwary999 <tiwarya9@gmail.com>
2020-06-07 16:41:18 +0530

    working on adding like action

--- WallofUFameReact/src/redux/reducers/HomeScreenReducer.js
+++ WallofUFameReact/src/redux/reducers/HomeScreenReducer.js
@@ -14,9 +14,24 @@ const homeScreenReducer = (state = initialState, action) => {
                 posts: payload
             }
 
+        case Actions.UPDATE_LIKE:
+            updateLike(state, payload)    
+
         default:
             return state;    
     }
+}
+
+const updateLike = (state, pos) => {
+    let post = state.posts[pos]
+    post.isLiked = (post.isLiked == 0?1:0)
+    let updatedPosts = state.posts
+    updatedPosts[pos] = post
+    // let updatedPosts = [state.posts.slice(0, pos), post, state.posts.slice(pos+1)]
+    for(let i=0; i<updatedPosts.length; i++){
+        console.log("post updated "+JSON.stringify(updatedPosts[i]))
+    }
+    return {...state, posts: updatedPosts}
 }
 
 export default homeScreenReducer
