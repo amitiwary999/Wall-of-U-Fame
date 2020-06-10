5ac848de0c3d650a0caa6b1c4e9bccfc2b599724
amitiwary999 <tiwarya9@gmail.com>
2020-06-10 23:40:52 +0530

    updating like, unlike action

--- WallofUFameReact/src/redux/reducers/HomeScreenReducer.js
+++ WallofUFameReact/src/redux/reducers/HomeScreenReducer.js
@@ -1,4 +1,5 @@
 import * as Actions from '../type'
+import { Item } from 'native-base';
 
 const initialState = {
     posts : []
@@ -15,7 +16,18 @@ const homeScreenReducer = (state = initialState, action) => {
             }
 
         case Actions.UPDATE_LIKE:
-            updateLike(state, payload)    
+          //  updateLike(state, payload)
+            let item = state.posts[payload]
+            item.isLiked = (item.isLiked == 0?1:0)
+            let newPost = [...state.posts.slice(0, payload), item, ...state.posts.slice(payload+1)]
+            return {...state, posts: newPost}
+        //   let item = state.posts[payload]
+        //   item.isLiked = (item.isLiked== 0?1:0)
+        //   return {...state,
+        //       [...state.posts.slice(0, payload),
+        //       item,
+        //       ...state.posts.slice(payload+1)]
+        //   }  
 
         default:
             return state;    
@@ -25,7 +37,7 @@ const homeScreenReducer = (state = initialState, action) => {
 const updateLike = (state, pos) => {
     let post = state.posts[pos]
     post.isLiked = (post.isLiked == 0?1:0)
-    let updatedPosts = state.posts
+    let updatedPosts = {...state.posts}
     updatedPosts[pos] = post
     // let updatedPosts = [state.posts.slice(0, pos), post, state.posts.slice(pos+1)]
     for(let i=0; i<updatedPosts.length; i++){
